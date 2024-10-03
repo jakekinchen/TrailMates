e315a68350fbda8dfb79d3689574998f13ecf175
Jake Kinchen <jake.kinchen@onetier.com>
2024-10-03 09:04:41 -0500

    Initial Commit

---
+++ TrailMatesTests/TrailMatesTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrailMatesTests.swift
+//  TrailMatesTests
+//
+//  Created by Jake Kinchen on 10/3/24.
+//
+
+import Testing
+@testable import TrailMates
+
+struct TrailMatesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
